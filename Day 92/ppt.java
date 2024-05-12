d25ed04cf71d9b86c32f58f69b7011f35e7e9e57
Prince <princejaiswal13206@gmail.com>
2024-05-12 22:30:28 +0530

    Day 92

---
+++ Day 92/ppt.java
@@ -0,0 +1,10 @@
+
+
+  public class StaticKeywordExample
+  {
+    private static int count = 0;
+    public static void printCount()
+    {
+      System.out.println("Number of Example objects created so far: " + count);
+    }
+  }
